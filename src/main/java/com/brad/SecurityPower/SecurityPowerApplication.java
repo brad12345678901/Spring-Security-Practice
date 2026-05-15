54c4536a7adb326ad47e1b32aaae4c81b9dbb49e
Brad <ibanezbradley@gmail.com>
2026-05-15 16:42:12 +0800

    Initial Commit

---
+++ src/main/java/com/brad/SecurityPower/SecurityPowerApplication.java
@@ -0,0 +1,13 @@
+package com.brad.SecurityPower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityPowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityPowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brad/SecurityPower/SecurityPowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brad.SecurityPower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityPowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
